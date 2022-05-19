59d43115acacb3d9fd10645e6b73c7ad6baac960
Harsha Nandan <50901784+HarshaNandan99@users.noreply.github.com>
2022-05-19 18:24:10 -0400

    Add files via upload

---
+++ src/CalculatorModule/__init__.py
@@ -0,0 +1,40 @@
+"""
+
+CalculatorModule
+--------------------
+This is a Module which has Classes for Calculator Operations.
+
+"""
+
+
+class Calculator(object):
+
+    """Calculator Class which is responsible for performing basic calculator operations"""
+
+    def add_numbers(num1, num2):
+        """
+            This is a string which adds two integers
+            :return: result
+        """
+        return num1 + num2
+
+    def subtract_numbers(num1, num2):
+        """
+            This is a string which subtracts two integers
+            :return: result
+        """
+        return num1 - num2
+
+    def multiply_numbers(num1, num2):
+        """
+            This is a string which multiplies two integers
+            :return: result
+        """
+        return num1 * num2
+
+    def divide_numbers(num1, num2):
+        """
+            This is a string which divides two integers
+            :return: result
+        """
+        return num1 / num2
